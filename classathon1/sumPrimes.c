47ca10e19722e0dc1c2ba07ab980dd8b1534c8d1
Zack Hartmann <zackhartmann@gmail.com>
2017-11-02 23:54:23 +0000

    style

--- classathon1/sumPrimes.c
+++ classathon1/sumPrimes.c
@@ -7,7 +7,8 @@ int sum_primes(int num);
 
 int main(int argc, char* argv[]) {
     if (!((argc == 2) && atoi(argv[1]) > 0)) {
-        printf("Usage: ./sumPrimes k\n"); return 1;
+        printf("Usage: ./sumPrimes k\n");
+        return 1;
     }
     int result = sum_primes(atoi(argv[1]));
     printf("%d\n", result);
@@ -16,7 +17,7 @@ int main(int argc, char* argv[]) {
 
 bool is_prime(int number) {
     for(int i = 2; i < number/2; i++) {
-        if (number % i == 0) { return false; }
+        if (number % i == 0) return false;
     }
     return true;
 }
@@ -25,7 +26,7 @@ int sum_primes(int num) {
     int total = 0;
 
     for(int j = 2; j <= num; j++) {
-        if (is_prime(j)) { total += j; }
+        if (is_prime(j)) total += j;
     }
     return total;
 }
