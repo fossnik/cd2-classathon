e274788036800b072fa0f9a745b91af7a2c679bc
Zack Hartmann <zackhartmann@gmail.com>
2017-11-02 23:15:28 +0000

    codeblock rearrange

--- classathon1/sumPrimes.c
+++ classathon1/sumPrimes.c
@@ -1,27 +1,11 @@
 // sumPrimes
-#include <stdbool.h>
+
 #include <stdio.h>
-#include <cs50.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-bool is_prime(int number) {
-    for(int i = 2; i < number/2; i++) {
-        if (number % i == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-int sum_primes(int num) {
-    int total = 0;
-
-    for(int j = 2; j <= num; j++) {
-        if (is_prime(j)) {
-            total = total + j;
-        }
-    }
-    return total;
-}
+bool is_prime(int number);
+int sum_primes(int num);
 
 int main(int argc, char* argv[]) {
     int seeknumber;
@@ -35,4 +19,20 @@ int main(int argc, char* argv[]) {
     int result = sum_primes(seeknumber);
     printf("%d\n", result);
     return result;
+}
+
+bool is_prime(int number) {
+    for(int i = 2; i < number/2; i++) {
+        if (number % i == 0) { return 0; }
+    }
+    return 1;
+}
+
+int sum_primes(int num) {
+    int total = 0;
+
+    for(int j = 2; j <= num; j++) {
+        if (is_prime(j)) { total += j; }
+    }
+    return total;
 }
